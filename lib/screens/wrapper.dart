0541c2705076f0c4c8570a97982a9523fe181c59
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 11:08:29 +0330

    Route stuff

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:learnfirebase/models/user.dart';
 import 'package:learnfirebase/screens/authenticate/authenticate.dart';
 import 'package:learnfirebase/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
-    // return Home();
+
+    final user = Provider.of<User>(context);
+    print(user);
+
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
+
   }
 }

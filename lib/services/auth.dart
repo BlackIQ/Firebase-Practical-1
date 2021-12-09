b9352a7a5076527bf1f90436c3cb391b9b2c6ee1
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 13:08:59 +0330

    Add login

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,8 +31,34 @@ class AuthService {
   }
 
   // Sign in with email
+  Future loginWithEmail(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // sign up with email
+  Future registerWithEmail(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   // sign out
   Future signOut() async {
@@ -43,5 +69,4 @@ class AuthService {
       return null;
     }
   }
-
 }

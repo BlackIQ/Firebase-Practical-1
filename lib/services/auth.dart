cc61f18efd13adeb509ce70026a14432648c1f48
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 10:10:17 +0330

    Working on anon signin

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:learnfirebase/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Create user object baised on FirebaseUser
+  User? _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
 
   // Sign in anon
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
       return null;

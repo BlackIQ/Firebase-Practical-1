47c07a9381c2e37e1f703cb052831b7f0509e0e2
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 11:08:01 +0330

    Using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:learnfirebase/models/user.dart';
 import 'package:learnfirebase/screens/wrapper.dart';
+import 'package:learnfirebase/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.brown,
+        ),
+        home: Wrapper(),
       ),
-      home: Wrapper(),
     );
   }
 }

30b9daec2c90309a0c6802293c6961ffa5febaaa
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 17:29:19 +0330

    Loadling an etc

---
+++ lib/shared/loading.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Platform.isIOS
+            ? CupertinoActivityIndicator()
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+}

2cbfcf7dcd08305bdfa91217ec085408b4d13283
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 09:30:03 +0330

    Render authenticate

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:learnfirebase/screens/home/home.dart';
+import 'package:learnfirebase/screens/authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return Authenticate();
   }
 }

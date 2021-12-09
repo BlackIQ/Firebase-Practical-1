b6e18dd62689b40c12d426e742b74d3967156d0d
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-09 09:29:33 +0330

    Update material

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
       home: Wrapper(),
     );
   }

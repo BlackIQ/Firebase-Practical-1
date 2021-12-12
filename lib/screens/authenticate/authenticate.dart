41da7ea0196e2948fbbfa540598617793ecb54e0
Amirhossein Mohamamdi <amirhosseinmohammadi1380@yahoo.com>
2021-12-12 14:18:07 +0330

    Fix small stuff

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -47,41 +47,41 @@ class _AuthenticateState extends State<Authenticate> {
               ),
             ),
             SizedBox(height: 50),
-            // Container(
-            //   height: 50,
-            //   child: ElevatedButton(
-            //     onPressed: () async {
-            //       dynamic result = await _auth.signInAnon();
-            //       if (result.runtimeType == List) {
-            //         showDialog(
-            //           context: context,
-            //           builder: (BuildContext context) {
-            //             return AlertDialog(
-            //               title: Text('Something went wrong'),
-            //               content: SingleChildScrollView(
-            //                 child: Text(
-            //                   result[1],
-            //                 ),
-            //               ),
-            //               actions: <Widget>[
-            //                 TextButton(
-            //                   child: Text('Ok'),
-            //                   onPressed: () {
-            //                     Navigator.of(context).pop();
-            //                   },
-            //                 ),
-            //               ],
-            //             );
-            //           },
-            //         );
-            //       } else {
-            //         Navigator.of(context).pop(context);
-            //       }
-            //     },
-            //     child: Text('Sign in Anonymously'),
-            //   ),
-            // ),
-            // SizedBox(height: 10),
+            Container(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () async {
+                  dynamic result = await _auth.signInAnon();
+                  if (result.runtimeType == List) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Something went wrong'),
+                          content: SingleChildScrollView(
+                            child: Text(
+                              result[1],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Ok'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    Navigator.of(context).pop(context);
+                  }
+                },
+                child: Text('Sign in Anonymously'),
+              ),
+            ),
+            SizedBox(height: 10),
             Container(
               height: 50,
               child: ElevatedButton(
